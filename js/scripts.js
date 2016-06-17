574f8aa9ea79ac43e2692f2784f98254f15c03c1
Bang Nguyen <bangnv77@gmail.com>
2016-06-17 16:29:20 -0700

    add and try out codes to calculate price

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,95 @@
-// Back-End logic
-function Pizza(type, size) {
-  this.type = type;
-  this.size = size;
+// business logic
+function Pizza(pizzaName, pizzaSize) {
+  this.pizzaName = pizzaName;
+  this.pizzaSize = pizzaSize;
   this.toppings = [];
 }
 
-function Topping(top1, top2, top3, top4){
-  this.top1 = top1;
-  this.top2 = top2;
-  this.top3 = top3;
-  this.top4 = top4;
+function Topping(topping1, topping2, topping3, topping4, topping5) {
+  this.pizzaTopping = topping1;
+  this.pizzaTopping = topping2;
+  this.pizzaTopping = topping3;
+  this.pizzaTopping = topping4;
+  this.pizzaTopping = topping5;
 }
 
-Pizza.prototype.typeSize = function() {
-  return this.type + " " + this.size;
+Pizza.prototype.nameSize = function() {
+  return this.pizzaName + " " + this.pizzaSize;
 }
 
-Toppings.prototype.fullToppings = function() {
-  return this.top1 + ", " + this.top2 + ", " + this.top3 + ", " + this.top4;
+Topping.prototype.fullTopping = function() {
+  return this.topping1 + " " +this.topping2 + " " + this.topping3 + " " + this.topping4 + " " + this.topping5;
 }
 
-// user interface logic
+Pizza.prototype.price = function(price) {
+  var price = 0;
+  if (pizzaSize === "Small".toLowerCase() && pizzaTopping === "Regular".toLowerCase) {
+    return price = 10;
+  } else if (pizzaSize === "Small".toLowerCase() && pizzaTopping === "anchovy".toLowerCase) {
+    return price = 11;
+    }
+    else if (pizzaSize === "Small".toLowerCase() && pizzaTopping === "artichoke".toLowerCase) {
+    return price = 12;
+    }
+    else if (pizzaSize === "Small".toLowerCase() && pizzaTopping === "cheese".toLowerCase) {
+    return price = 12;
+    }
+    else if (pizzaSize === "Small".toLowerCase() && pizzaTopping === "pepperroni".toLowerCase) {
+    return price = 11;
+    }
+    else if (pizzaSize === "medium".toLowerCase() && pizzaTopping === "Regular".toLowerCase) {
+    return price = 16;
+    }
+    else if (pizzaSize === "medium".toLowerCase() && pizzaTopping === "anchovy".toLowerCase) {
+    return price = 17;
+    }
+    else if (pizzaSize === "medium".toLowerCase() && pizzaTopping === "artichoke".toLowerCase) {
+    return price = 18;
+    }
+    else if (pizzaSize === "medium".toLowerCase() && pizzaTopping === "cheese".toLowerCase) {
+    return price = 18;
+    }
+    else if (pizzaSize === "medium".toLowerCase() && pizzaTopping === "pepperroni".toLowerCase) {
+    return price = 17;
+    }
+    else if (pizzaSize === "large".toLowerCase() && pizzaTopping === "Regular".toLowerCase) {
+    return price = 20;
+    }
+    else if (pizzaSize === "large".toLowerCase() && pizzaTopping === "anchovy".toLowerCase) {
+    return price = 21;
+    }
+    else if (pizzaSize === "large".toLowerCase() && pizzaTopping === "artichoke".toLowerCase) {
+    return price = 22;
+    }
+    else if (pizzaSize === "large".toLowerCase() && pizzaTopping === "cheese".toLowerCase) {
+    return price = 22;
+    }
+    else if (pizzaSize === "large".toLowerCase() && pizzaTopping === "pepperroni".toLowerCase) {
+    return price = 21;
+    }
+    else {
+    return price;
+    }
+};
+
 $(document).ready(function() {
   $("form#selection").submit(function(event) {
     event.preventDefault();
 
-    var pizzaType = $("select#selectPizzas").val();
+    var pizzaName = $("select#selectPizzas").val();
     var pizzaSize = $("select#selectSizes").val();
-    var pizzaToppings = $("div#selectToppings").val();
-    var newPizza = new Pizza(pizzaType, pizzaSize);
-    var newTopping = new Topping(top1, top2, top3, top4);
+    var pizzaTopping = $("select#selectToppings :selected").val();
+    var pizzaOrder = new Pizza(pizzaName, pizzaSize);
+    var toppingOrder = new Topping(pizzaTopping);
 
-    $("ul#pizzaList").append("<li><span class='pizzaItems'>" + newPizza.typeSize() + "</span></li>");
+    $("ul#pizzaList").append("<li><span class='pizzaItems>" + Pizza.nameSize() + "</span></li>");
+    $("ul#pizzaList").append("<li><span class='pizzaItems>" + Topping.fullTopping() + "</span></li>");
 
-
-    $(".pizzaList").last().click(function() {
-    $("#showFinalSelection").show();
-    $("#show-contact h2").text(newPizza.typeSize);
-
-    $("ul#addresses").text("");
-    newContact.addresses.forEach(function(address) {
-      $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
+    $("#placeOrder").click(function() {
+      $(".order").show();
+      $(".order h2").text(newPizza.nameSize());
+      $(".order .toppings").text(fullTopping);
+      $(".order .price").text(Pizza.toppings);
     });
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
   });
 });
