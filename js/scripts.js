402075fccaf854100dfe2422c89d5b7fd5b8b2b3
Bang Nguyen <bangnv77@gmail.com>
2016-06-23 11:59:27 -0700

    final fix for price calculation

--- js/scripts.js
+++ js/scripts.js
@@ -3,44 +3,36 @@ function Pizza(pizzaName, pizzaSize, pizzaTopping) {
   this.pizzaName = pizzaName;
   this.pizzaSize = pizzaSize;
   this.pizzaTopping = pizzaTopping;
-  // this.pizzaPrice = pizzaPrice;
 }
 
-// function Price(size, pizzaPrice) {
-//   this.size = size;
-//   this.pizzaPrice = pizzaPrice;
-// }
-
-Pizza.prototype.price = function() {
-  if (pizzaSize ===  "large") {
+Pizza.prototype.calculatePrice = function() {
+  if (this.pizzaSize ===  "large") {
     this.price = 20;
-  } else if (pizzaSize === "medium") {
+  } else if (this.pizzaSize === "medium") {
     this.price = 16;
-  } else if (pizzaSize === "small") {
+  } else if (this.pizzaSize === "small") {
     this.price = 10;
   }
-  for (i = 0; i <= pizzaTopping.length; i++) {
-    if (pizzaTopping[i] === 'anchovy' || pizzaTopping[i] === 'artichoke') {
+  for (i = 0; i <= this.pizzaTopping.length; i++) {
+    if (this.pizzaTopping[i] === 'anchovy' || this.pizzaTopping[i] === 'artichoke') {
       this.price += 1;
-    } else if (pizzaTopping[i] === 'cheese' || pizzaTopping[i] === 'pepperroni') {
+    } else if (this.pizzaTopping[i] === 'cheese' || this.pizzaTopping[i] === 'pepperroni') {
       this.price += 2;
     }
   }
-  // return price;
 }
 
 $(document).ready(function() {
   $("#selection").submit(function(event) {
-
     event.preventDefault();
 
     var pizzaName = $("select#selectPizzas").val();
     var pizzaSize = $("select#selectSizes").val();
     var pizzaTopping = $("input:checkbox:checked").map(function () {
      return $(this).val()
-   }).get();
+    }).get();
     var pizzaOrder = new Pizza(pizzaName, pizzaSize, pizzaTopping);
-    // var totalPrice = Pizza.pizzaPrice
+    pizzaOrder.calculatePrice();
 
     $("#reviewOrder").show();
     $("ul#pizzaList").append("<li><span class='pizzaItems'>" + pizzaOrder.pizzaName + ", " + "</span></li>" + "<li><span class='pizzaItems'>" + pizzaOrder.pizzaSize + "</span></li>"+ "<li><span class='pizzaItems'>" + pizzaOrder.price + "</span></li>");
